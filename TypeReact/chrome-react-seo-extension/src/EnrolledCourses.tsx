87c2c47d1b05bf305320fe3f69258b7efd8ccaa7
Madelen <madelenf143@berkeley.edu>
2023-03-26 00:45:26 -0700

    added comments

--- TypeReact/chrome-react-seo-extension/src/EnrolledCourses.tsx
+++ TypeReact/chrome-react-seo-extension/src/EnrolledCourses.tsx
@@ -8,12 +8,13 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import { CardActionArea } from '@mui/material';
+import CardHeader from '@mui/material/CardHeader';
 
 export default function EnrolledCourses() {
     
     const courseList = [
-        {name:"Kyle Hanagami", description: "build a foundation"},
-        {name:"Koharu", description: "understanding body-awareness"},
+        {name:"Kyle Hana", description: "build a foundation"},
+        {name:"Koharu", description: "understand body-awareness"},
         {name:"Bam Martin", description: "improve footwork"},
         {name:"Bailey Sok", description: "timing and control"},
         {name:"Nick Joseph", description: "add power"},
@@ -29,16 +30,14 @@ export default function EnrolledCourses() {
             <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start' }}>
 
                 {courseList.map((course) => 
-                    
+                    // TODO: FIX MARGINS AND WIDTH
                     // *textDecor= none gets rids of underline from text
                     <Link to={path} style={{ textDecoration: 'none' }}>
-                        <Card variant="outlined" sx= {{padding: 1, margin: 1.5, width: 200, height: 120}}>
+                        <Card  sx= {{margin: 1.5, width: 200, height: 120}}>
                             {/* //! this makes the whole card clickable */}
                             <CardActionArea>
                                 <CardContent>
-                                    <Typography variant="h6" color="text.primary" gutterBottom>
-                                    {course.name}
-                                    </Typography> 
+                                    <CardHeader sx={{paddingTop:0}} title= {course.name} /> 
                                     <Typography variant="body2" color="text.secondary" gutterBottom>
                                     {course.description}
                                     </Typography>   
